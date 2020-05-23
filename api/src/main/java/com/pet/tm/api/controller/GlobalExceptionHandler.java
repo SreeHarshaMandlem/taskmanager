5dca0d2e5ea8e7e3a72602d78e0a847918329403
sree.mandlem@diconium.com <ChalQchal0chal>
2020-05-23 23:56:23 +0200

    #6 REST API for "Creating a New User" ---> Add new validation Exception and handler

--- api/src/main/java/com/pet/tm/api/controller/GlobalExceptionHandler.java
+++ api/src/main/java/com/pet/tm/api/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.pet.tm.api.controller;
 
 import com.pet.tm.api.dto.ErrorDto;
+import com.pet.tm.api.exception.InvalidPropertyValueException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,33 +13,48 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	private static Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+  private static Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-	/**
-	 * Handles {@link HttpRequestMethodNotSupportedException} types.
-	 *
-	 * @param exception of type HttpRequestMethodNotSupportedException
-	 * @return ErrorDto
-	 */
-	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	public ErrorDto handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
-		return ErrorDto.builder()
-				.code(HttpStatus.METHOD_NOT_ALLOWED.value())
-				.message(exception.getMessage())
-				.build();
-	}
+  /**
+   * Handles {@link InvalidPropertyValueException} types.
+   *
+   * @param exception of type InvalidPropertyValueException
+   * @return ErrorDto
+   */
+  @ExceptionHandler(InvalidPropertyValueException.class)
+  public ErrorDto handleInvalidPropertyValueException(InvalidPropertyValueException exception) {
+    return ErrorDto.builder()
+        .code(HttpStatus.BAD_REQUEST.value())
+        .message(exception.getMessage())
+        .build();
+  }
 
-	/**
-	 * Handles {@link MethodArgumentNotValidException} types.
-	 *
-	 * @param exception of type MethodArgumentNotValidException
-	 * @return ErrorDto
-	 */
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ErrorDto handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-		return ErrorDto.builder()
-				.code(HttpStatus.PRECONDITION_FAILED.value())
-				.message(exception.getMessage())
-				.build();
-	}
+  /**
+   * Handles {@link HttpRequestMethodNotSupportedException} types.
+   *
+   * @param exception of type HttpRequestMethodNotSupportedException
+   * @return ErrorDto
+   */
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ErrorDto handleHttpRequestMethodNotSupportedException(
+      HttpRequestMethodNotSupportedException exception) {
+    return ErrorDto.builder()
+        .code(HttpStatus.METHOD_NOT_ALLOWED.value())
+        .message(exception.getMessage())
+        .build();
+  }
+
+  /**
+   * Handles {@link MethodArgumentNotValidException} types.
+   *
+   * @param exception of type MethodArgumentNotValidException
+   * @return ErrorDto
+   */
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ErrorDto handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    return ErrorDto.builder()
+        .code(HttpStatus.PRECONDITION_FAILED.value())
+        .message(exception.getMessage())
+        .build();
+  }
 }
---
+++ api/src/main/java/com/pet/tm/api/exception/InvalidPropertyValueException.java
@@ -0,0 +1,8 @@
+package com.pet.tm.api.exception;
+
+public class InvalidPropertyValueException extends RuntimeException {
+
+  public InvalidPropertyValueException(String message) {
+    super(message);
+  }
+}
