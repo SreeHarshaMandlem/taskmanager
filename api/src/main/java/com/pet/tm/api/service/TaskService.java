816761831b4b8bf1ac7e76e5d6e8e971b8599927
sree.mandlem@diconium.com <ChalQchal0chal>
2020-05-24 10:56:07 +0200

    #6 REST API for "Creating a New User" --> Fix review comments

--- api/src/main/java/com/pet/tm/api/service/TaskService.java
+++ api/src/main/java/com/pet/tm/api/service/TaskService.java
@@ -20,14 +20,14 @@ public class TaskService {
   }
 
   public TaskEntity createTask(@NonNull TaskEntity task) {
-  	validteTask(task);
+    validteTask(task);
     LOG.info("Creating Task: \n \t\t{}", task);
     return taskRepository.save(task);
   }
 
-	private void validteTask(TaskEntity task) {
-		if (task.getAssignee() != null && task.getAssignee().getId() == null) {
-			throw new InvalidPropertyValueException("Assignee's id cannot be null.");
-		}
-	}
+  private void validteTask(TaskEntity task) {
+    if (task.getAssignee() != null && task.getAssignee().getId() == null) {
+      throw new InvalidPropertyValueException("Assignee's id cannot be null.");
+    }
+  }
 }
