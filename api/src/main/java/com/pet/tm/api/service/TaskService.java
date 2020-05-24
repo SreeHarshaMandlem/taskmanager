2939c1a2f19482d4a8e390c840f125883eded4ae
sree.mandlem@diconium.com <ChalQchal0chal>
2020-05-24 12:14:00 +0200

    #14 Fetch Tasks

--- api/src/main/java/com/pet/tm/api/service/TaskService.java
+++ api/src/main/java/com/pet/tm/api/service/TaskService.java
@@ -2,11 +2,14 @@ package com.pet.tm.api.service;
 
 import com.pet.tm.api.entity.TaskEntity;
 import com.pet.tm.api.exception.InvalidPropertyValueException;
+import com.pet.tm.api.exception.TaskNotFoundException;
 import com.pet.tm.api.repository.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -29,5 +32,18 @@ public class TaskService {
     if (task.getAssignee() != null && task.getAssignee().getId() == null) {
       throw new InvalidPropertyValueException("Assignee's id cannot be null.");
     }
+  }
+
+  public List<TaskEntity> getAllTasks() {
+    return taskRepository.findAll();
+  }
+
+  public TaskEntity getTaskById(@NonNull Long id) {
+    return taskRepository
+        .findById(id)
+        .orElseThrow(
+            () -> {
+              throw new TaskNotFoundException("Task with id: " + id + "does not exist");
+            });
   }
 }
