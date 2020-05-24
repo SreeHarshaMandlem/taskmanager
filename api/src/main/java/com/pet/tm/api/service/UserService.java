5a2b196a23a878308ffeacf14febccb0144c0ddf
sree.mandlem@diconium.com <ChalQchal0chal>
2020-05-24 14:37:41 +0200

    #16 Fetch Users

--- api/src/main/java/com/pet/tm/api/service/UserService.java
+++ api/src/main/java/com/pet/tm/api/service/UserService.java
@@ -1,11 +1,15 @@
 package com.pet.tm.api.service;
 
 import com.pet.tm.api.entity.UserEntity;
+import com.pet.tm.api.exception.TaskNotFoundException;
+import com.pet.tm.api.exception.UserNotFoundException;
 import com.pet.tm.api.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,5 +25,18 @@ public class UserService {
   public UserEntity createUser(@NonNull UserEntity user) {
     LOG.info("Creating User: \n \t\t{}", user);
     return userRepository.save(user);
+  }
+
+  public List<UserEntity> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  public UserEntity getUserById(@NonNull Long id) {
+	  return userRepository
+			  .findById(id)
+			  .orElseThrow(
+					  () -> {
+						  throw new UserNotFoundException("User with id: " + id + " does not exist");
+					  });
   }
 }
